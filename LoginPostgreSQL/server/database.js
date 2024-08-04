9e4b3aed3b2b3b3a63548d3e62c4fb604c3541b4
Matt-Gary <matgarbrasil@gmail.com>
2024-08-04 14:53:01 -0300

    Creating cards

--- LoginPostgreSQL/server/database.js
+++ LoginPostgreSQL/server/database.js
@@ -1,5 +1,4 @@
 const {Pool} = require("pg")
-const {response} = require("express")
 
 const pool = new Pool({
     user:"postgres",
@@ -7,19 +6,6 @@ const pool = new Pool({
     host: "localhost",
     port: 5432,
     database:"yt_login_system"
-}
-)
-const createTableQry = `CREATE TABLE accounts (
-    user_id serial PRIMARY KEY,
-    username VARCHAR (50) UNIQUE NOT NULL,
-    password VARCHAR (50) UNIQUE NOT NULL);`
-
-pool.query(createTableQry).then((Response) => {
-    console.log("Table created")
-    console.log(response)
-})
-.catch((err)=> {
-    console.log(err)
 })
 
-module.exports = pool
+module.exports = pool;
--- LoginPostgreSQL/server/server.js
+++ LoginPostgreSQL/server/server.js
@@ -1,16 +1,38 @@
+
+
 const express = require("express")
 const cors = require("cors")
+const pool = require("./database")
+
 
 const app = express()
 
 app.use(express.json()) //if server receive json request it say to server that it's json request
 app.use(cors())  // corse make sure it's save localhost:3000- app server - and server localhost:4000
+app.use((req, res, next) => {
+    res.setHeader("Content-Security-Policy", "default-src 'self'; script-src 'self'; img-src 'self' http://localhost:4000;");
+    next();
+});
 
 //define endpoint
-app.get("/adduser", (req,res) => {
-    console.log(req.body)
-    res.send("Respose received: " + req.body)
-})
+app.post("/adduser", async (req, res) => {
+    const { username, password } = req.body; // Destructure for easier access
+
+    // For security, never directly insert user inputs into your SQL queries. Use parameterized queries instead.
+    const insertSTM = `INSERT INTO accounts (username, password) VALUES('$1', '$2');`;
+;
+
+    try {
+        const response = await pool.query(insertSTM, [username, password]);
+        console.log("Data Saved", response);
+        res.status(201).send("User added");
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Server error");
+    }
+});
+
+
 
 
 app.listen(4000, () => console.log("Server on localhost:4000"))
