5f4747fb7a42efde7eee2a3d1554de15eb37c473
Matt-Gary <matgarbrasil@gmail.com>
2024-08-04 14:55:11 -0300

    Creating first cards

---
+++ full-stack-app/server/index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+app.use(express.json()) //allow to access the body being parse (send in json)
+app.use(cors())
+
+
+const db = require('./models') //connect database
+
+
+//Routers
+const postRouter = require('./routes/Posts')
+app.use("/posts",postRouter)
+
+
+
+db.sequelize.sync().then(() => {
+    app.listen(4000, () => console.log("Server on localhost:4000"))
+})
